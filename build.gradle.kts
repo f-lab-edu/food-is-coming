996874866922d1959fac684bc26632eb8a91fc91
msugo1 <msugo1@github.com>
2021-06-09 21:42:55 +0900

    fix: downgrade the spring boot version
    * reflect on the feedback
    
    - 스프링 부트 2.5.1-SNAPSHOT -> 2.5.0 으로 Downgrade
    
    - `SNAPSHOT`의 의미는 현재 개발중이라는 뜻으로 불안정한 버전을 의미한다.
    
    - 따라서 버전을 다운그레이드 하고 불필요한 의존성들을 삭제
    
    Resolves: #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.1-SNAPSHOT"
+    id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.spring") version "1.5.10"
@@ -14,8 +14,6 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
