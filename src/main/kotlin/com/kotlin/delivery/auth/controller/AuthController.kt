00d344173f518c9ab420c9b0ec6c479192725d8f
msugo1 <gomsu1045@gmail.com>
2021-08-15 16:26:30 +0900

    ktlint 설정 추가 및 체크 반영

--- src/main/kotlin/com/kotlin/delivery/auth/controller/AuthController.kt
+++ src/main/kotlin/com/kotlin/delivery/auth/controller/AuthController.kt
@@ -4,7 +4,12 @@ import com.kotlin.delivery.auth.dto.TokenSet
 import com.kotlin.delivery.auth.service.AuthService
 import com.kotlin.delivery.auth.service.JwtTokenService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/members/common")
